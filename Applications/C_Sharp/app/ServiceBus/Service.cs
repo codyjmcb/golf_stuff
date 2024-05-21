8bc3b5ed7c68fd80c935e128fcbbfc1892ef8765
Cody Windows Machine <cody.j.mcbride@gmail.com>
2024-05-21 12:25:42 -0400

    initial csharp application

---
+++ Applications/C_Sharp/app/ServiceBus/Service.cs
@@ -0,0 +1,61 @@
+﻿using DataModels;
+using Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace ServiceBus
+{
+    public class Service
+    {
+        IRepository<Golfer> _golferRepo;
+
+        public Service(IRepository<Golfer> golferRepo)
+        {
+            _golferRepo = golferRepo;
+        }
+
+        public List<Golfer> FindAllGolfers()
+        {
+            List<Golfer> golferList = (List<Golfer>)_golferRepo.FindAll();
+
+            return golferList;
+        }
+
+        public Golfer FindGolfer(string id)
+        {
+            Golfer golfer = _golferRepo.Find(id);
+
+            return golfer;
+        }
+
+        public bool DeleteGolfer(string id)
+        {
+            if (_golferRepo.Delete(id))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool AddGolfer(Golfer x)
+        {
+            if (_golferRepo.Add(x))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool UpdateGolfer(Golfer x)
+        {
+            if (_golferRepo.Update(x))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Applications/C_Sharp/app/app/frmMain.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace app
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetAllGolfers = new System.Windows.Forms.Button();
+            this.btnGetGolfer = new System.Windows.Forms.Button();
+            this.btnAddGolfer = new System.Windows.Forms.Button();
+            this.btnUpdateGolfer = new System.Windows.Forms.Button();
+            this.btnDeleteGolfer = new System.Windows.Forms.Button();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGetAllGolfers
+            // 
+            this.btnGetAllGolfers.Location = new System.Drawing.Point(119, 156);
+            this.btnGetAllGolfers.Name = "btnGetAllGolfers";
+            this.btnGetAllGolfers.Size = new System.Drawing.Size(103, 23);
+            this.btnGetAllGolfers.TabIndex = 0;
+            this.btnGetAllGolfers.Text = "Get All Golfers";
+            this.btnGetAllGolfers.UseVisualStyleBackColor = true;
+            this.btnGetAllGolfers.Click += new System.EventHandler(this.btnGetAllGolfers_Click);
+            // 
+            // btnGetGolfer
+            // 
+            this.btnGetGolfer.Location = new System.Drawing.Point(119, 185);
+            this.btnGetGolfer.Name = "btnGetGolfer";
+            this.btnGetGolfer.Size = new System.Drawing.Size(103, 23);
+            this.btnGetGolfer.TabIndex = 1;
+            this.btnGetGolfer.Text = "Get Golfer";
+            this.btnGetGolfer.UseVisualStyleBackColor = true;
+            this.btnGetGolfer.Click += new System.EventHandler(this.btnGetGolfer_Click);
+            // 
+            // btnAddGolfer
+            // 
+            this.btnAddGolfer.Location = new System.Drawing.Point(119, 214);
+            this.btnAddGolfer.Name = "btnAddGolfer";
+            this.btnAddGolfer.Size = new System.Drawing.Size(103, 23);
+            this.btnAddGolfer.TabIndex = 2;
+            this.btnAddGolfer.Text = "Add Golfer";
+            this.btnAddGolfer.UseVisualStyleBackColor = true;
+            this.btnAddGolfer.Click += new System.EventHandler(this.btnAddGolfer_Click);
+            // 
+            // btnUpdateGolfer
+            // 
+            this.btnUpdateGolfer.Location = new System.Drawing.Point(119, 243);
+            this.btnUpdateGolfer.Name = "btnUpdateGolfer";
+            this.btnUpdateGolfer.Size = new System.Drawing.Size(103, 23);
+            this.btnUpdateGolfer.TabIndex = 3;
+            this.btnUpdateGolfer.Text = "Update Golfer";
+            this.btnUpdateGolfer.UseVisualStyleBackColor = true;
+            this.btnUpdateGolfer.Click += new System.EventHandler(this.btnUpdateGolfer_Click);
+            // 
+            // btnDeleteGolfer
+            // 
+            this.btnDeleteGolfer.Location = new System.Drawing.Point(119, 272);
+            this.btnDeleteGolfer.Name = "btnDeleteGolfer";
+            this.btnDeleteGolfer.Size = new System.Drawing.Size(103, 23);
+            this.btnDeleteGolfer.TabIndex = 4;
+            this.btnDeleteGolfer.Text = "Delete Golfer";
+            this.btnDeleteGolfer.UseVisualStyleBackColor = true;
+            this.btnDeleteGolfer.Click += new System.EventHandler(this.btnDeleteGolfer_Click);
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(247, 156);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.Size = new System.Drawing.Size(196, 139);
+            this.txtResponse.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.btnDeleteGolfer);
+            this.Controls.Add(this.btnUpdateGolfer);
+            this.Controls.Add(this.btnAddGolfer);
+            this.Controls.Add(this.btnGetGolfer);
+            this.Controls.Add(this.btnGetAllGolfers);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetAllGolfers;
+        private System.Windows.Forms.Button btnGetGolfer;
+        private System.Windows.Forms.Button btnAddGolfer;
+        private System.Windows.Forms.Button btnUpdateGolfer;
+        private System.Windows.Forms.Button btnDeleteGolfer;
+        private System.Windows.Forms.TextBox txtResponse;
+    }
+}
+
